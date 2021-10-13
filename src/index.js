806b10a3d76e1bae06066cfcd0f520b17a5cf6d2
Osborn <jc7483@nyu.edu>
2021-10-13 14:58:33 -0400

    [left nav][transaction page] implementation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <>
+    <CssBaseline />
+    <App />
+  </>,
+  rootElement
+);
