77403b127f08a22c498993ed663e1e6e34b8682e
Yixin Zhang <yz2255@cornell.edu>
2022-04-18 21:59:44 -0400

    remove onboarding

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import MakeNewDonation from "./pages/Donation/MakeNewDonation";
 import { useAuth } from "./contexts/AuthContext";
 import UploadImage from "./pages/RequestDonation/UploadImage";
 function App() {
-  const { role } = useAuth();
+  // const { role } = useAuth();
   // console.log(role);
 
   return (
@@ -32,46 +32,14 @@ function App() {
         <Switch>
           <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
-          <Route path="/onboard" component={Onboard} />
-          <PrivateRoute>
-            <MainLayout>
-              {role === "Vendor" && (
-                <Vendor>
-                  <Route exact path="/" component={HomePage} />
-                  <Route exact path="/faq" component={FAQ} />
-                  <Route exact path="/transaction" component={FAQ} />
-                  <Route exact path="/request" component={RequestDonation} />
-                  <Route path="/donation" component={CommunityPage} />
-                  <Route exact path="/profile" component={Profile} />
-                </Vendor>
-              )}
-              {role === "Requester" && (
-                <Requester>
-                  <Route exact path="/" component={HomePage} />
-                  <Route exact path="/faq" component={FAQ} />
-                  <Route exact path="/transaction" component={FAQ} />
-                  <Route exact path="/request" component={RequestDonation} />
-                  <Route path="/donation" component={CommunityPage} />
-                  <Route exact path="/profile" component={Profile} />
-                </Requester>
-              )}
-              {role === "Donor" && (
-                <Donor>
-                  <Route exact path="/" component={HomePage} />
-                  <Route exact path="/faq" component={FAQ} />
-                  <Route exact path="/transaction" component={FAQ} />
-                  <Route exact path="/request" component={RequestDonation} />
-                  <Route path="/donation" component={CommunityPage} />
-                  <Route exact path="/profile" component={Profile} />
-                  <Route
-                    exact
-                    path="/make-a-donation"
-                    component={MakeNewDonation}
-                  />
-                </Donor>
-              )}
-            </MainLayout>
-          </PrivateRoute>
+          <MainLayout>
+            <Route exact path="/" component={HomePage} />
+            {/* <Route exact path="/faq" component={FAQ} />
+            <Route exact path="/transaction" component={FAQ} /> */}
+            <Route exact path="/request" component={RequestDonation} />
+            <Route path="/donation" component={CommunityPage} />
+            <Route exact path="/profile" component={Profile} />
+          </MainLayout>
         </Switch>
       </div>
     </div>
