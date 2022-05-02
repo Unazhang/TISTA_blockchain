988c12ef4984ba7e834bb70c1fce787e13cb2a64
Zhou, Yun <yzhou776@gatech.edu>
2022-05-01 23:26:28 -0400

    Added images to login and sign up page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { Form, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Grid, CardMedia } from "@mui/material";
 
 export default function Login() {
   const emailRef = useRef();
@@ -29,35 +29,63 @@ export default function Login() {
 
   return (
     <div>
-      <Box sx={{ ml: 60, mr: 60, mt: 10 }}>
-        <Card>
-          <Card.Body>
-            <h2 className="text-center mb-4">Log In</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form onSubmit={handleSubmit}>
-              <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" ref={emailRef} required />
-              </Form.Group>
-              <Form.Group id="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" ref={passwordRef} required />
-              </Form.Group>
-              <Button
-                disabled={loading}
-                className="w-100"
-                type="submit"
-                variant="contained"
+      <Box sx={{ ml: 40, mr: 40, mt: 15 }}>
+        <Grid>
+          <Grid container wrap="nowrap">
+            <Grid item xs={8}>
+              <CardMedia
+                mediaStyle={{
+                  height: "50px",
+                  width: "100%",
+                  // top: "20px",
+                  // left: "20px",
+                  // right: "20px",
+                  // paddingTop: "56.25%",
+                }}
               >
-                Log In
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
+                <img
+                  src="https://www.diariodemarratxi.com/wp-content/uploads/2018/10/sangre-corazon-manos.jpg"
+                  style={{ height: "330px", width: "100%" }}
+                />
+              </CardMedia>
+            </Grid>
+            <Grid item xs={8}>
+              {" "}
+              <Card>
+                <Card.Body>
+                  <h2 className="text-center mb-4">Sign in to TISTA</h2>
+                  {error && <Alert variant="danger">{error}</Alert>}
+                  <Form onSubmit={handleSubmit}>
+                    <Form.Group id="email">
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control type="email" ref={emailRef} required />
+                    </Form.Group>
+                    <Form.Group id="password">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control
+                        type="password"
+                        ref={passwordRef}
+                        required
+                      />
+                    </Form.Group>
+                    <Button
+                      disabled={loading}
+                      className="w-100"
+                      type="submit"
+                      variant="contained"
+                    >
+                      Log In
+                    </Button>
+                  </Form>
+                  <div className="w-100 text-center mt-2">
+                    Need an account? <Link to="/signup">Sign Up</Link>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
       </Box>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </div>
     </div>
   );
 }
--- src/pages/Login/Signup.jsx
+++ src/pages/Login/Signup.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 import { Form, Card, Alert, ButtonGroup } from "react-bootstrap";
 import { useAuth } from "../../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Grid, CardMedia } from "@mui/material";
 
 export default function SignUp() {
   const emailRef = useRef();
@@ -50,89 +50,116 @@ export default function SignUp() {
 
   return (
     <div>
-      <Box sx={{ ml: 60, mr: 60, mt: 10 }}>
-        <Card>
-          <Card.Body>
-            <h2 className="text-center mb-4">Sign Up</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form onSubmit={handleSubmit}>
-              <Form.Group id="name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="text" ref={nameRef} required></Form.Control>
-              </Form.Group>
-              <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  ref={emailRef}
-                  required
-                ></Form.Control>
-              </Form.Group>
-              <Form.Group id="password">
-                <Form.Label>
-                  Password (must be at least 6 characters long)
-                </Form.Label>
-                <Form.Control
-                  type="password"
-                  ref={passwordRef}
-                  required
-                ></Form.Control>
-              </Form.Group>
-              <Form.Group id="password-confirm">
-                <Form.Label>Password Confirmation</Form.Label>
-                <Form.Control
-                  type="password"
-                  ref={passwordConfirmRef}
-                  required
-                ></Form.Control>
-              </Form.Group>
-              <ButtonGroup
-                id="rolebuttons"
-                className="form-group d-flex justify-content-between"
+      <Box sx={{ ml: 25, mr: 25, mt: 15 }}>
+        <Grid>
+          <Grid container wrap="nowrap">
+            <Grid item xs={8}>
+              <CardMedia
+                mediaStyle={{
+                  height: "80px",
+                  width: "100%",
+                  // top: "20px",
+                  // left: "20px",
+                  // right: "20px",
+                  // paddingTop: "56.25%",
+                }}
               >
-                <Button
-                  className="mr-1"
-                  type="button"
-                  id="Donor"
-                  onClick={handleClick}
-                  variant="outlined"
-                >
-                  Donor
-                </Button>
-                <Button
-                  className="mx-2"
-                  type="button"
-                  id="Requester"
-                  onClick={handleClick}
-                  variant="outlined"
-                >
-                  Requester
-                </Button>
-                <Button
-                  className="ml-1"
-                  type="button"
-                  id="Vendor"
-                  onClick={handleClick}
-                  variant="outlined"
-                >
-                  Vendor
-                </Button>
-              </ButtonGroup>
-              <Button
-                disabled={loading}
-                className="w-100"
-                type="submit"
-                variant="contained"
-              >
-                Sign Up
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
+                <img
+                  src="https://blog.delusionmfg.com/wp-content/uploads/2020/03/shape-your-beret-the-right-way.jpg"
+                  style={{ height: "600px", width: "100%" }}
+                />
+              </CardMedia>
+            </Grid>
+            <Grid item xs={8}>
+              <Card style={{ height: "100%", width: "100%" }}>
+                <Card.Body>
+                  <h2 className="text-center mb-4">Sign up to TISTA</h2>
+                  {error && <Alert variant="danger">{error}</Alert>}
+                  <Form onSubmit={handleSubmit}>
+                    <Form.Group id="name">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        ref={nameRef}
+                        required
+                      ></Form.Control>
+                    </Form.Group>
+                    <Form.Group id="email">
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control
+                        type="email"
+                        ref={emailRef}
+                        required
+                      ></Form.Control>
+                    </Form.Group>
+                    <Form.Group id="password">
+                      <Form.Label>
+                        Password (must be at least 6 characters long)
+                      </Form.Label>
+                      <Form.Control
+                        type="password"
+                        ref={passwordRef}
+                        required
+                      ></Form.Control>
+                    </Form.Group>
+                    <Form.Group id="password-confirm">
+                      <Form.Label>Password Confirmation</Form.Label>
+                      <Form.Control
+                        type="password"
+                        ref={passwordConfirmRef}
+                        required
+                      ></Form.Control>
+                    </Form.Group>
+                    <ButtonGroup
+                      id="rolebuttons"
+                      className="form-group d-flex justify-content-between"
+                    >
+                      <Button
+                        className="mr-1"
+                        type="button"
+                        id="Donor"
+                        onClick={handleClick}
+                        variant="outlined"
+                      >
+                        Donor
+                      </Button>
+                      <Button
+                        className="mx-2"
+                        type="button"
+                        id="Requester"
+                        onClick={handleClick}
+                        variant="outlined"
+                      >
+                        Requester
+                      </Button>
+                      <Button
+                        className="ml-1"
+                        type="button"
+                        id="Vendor"
+                        onClick={handleClick}
+                        variant="outlined"
+                      >
+                        Vendor
+                      </Button>
+                    </ButtonGroup>
+                    <Button
+                      disabled={loading}
+                      className="w-100"
+                      type="submit"
+                      variant="contained"
+                    >
+                      Sign Up
+                    </Button>
+                  </Form>
+                  <div className="w-100 text-center mt-2">
+                    Already have an account? <Link to="/login">Log In</Link>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
       </Box>
-      <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
-      </div>
     </div>
   );
 }
