94d2868ad75d7260d197cb80ee67e94c2036b62c
Yixin Zhang <yz2255@cornell.edu>
2022-05-03 11:05:43 -0400

    fix empty requirerole error

--- src/pages/RequireRole.js
+++ src/pages/RequireRole.js
@@ -2,18 +2,12 @@ import { useAuth } from "../contexts/AuthContext";
 
 function RequireRole({ requiredRole, children }) {
   const { role } = useAuth();
-  console.log("requirerole", requiredRole);
-  console.log("currentrole", role);
+  // console.log("requirerole", requiredRole);
+  // console.log("currentrole", role);
 
-  // if not signed in/ signed up, don't show
-  // if (requiredRole.length === 0 && currentUser === null) { return null }
-
-  // const access = requiredRole.some((item) => {
-  //   return item === role;
-  // });
   let access;
 
-  if (role.length < 0) {
+  if (!role || role.length < 0) {
     access = false;
   } else {
     access = role.some((item) => {
@@ -21,7 +15,7 @@ function RequireRole({ requiredRole, children }) {
     });
   }
 
-  console.log(access);
+  // console.log(access);
 
   return access ? { ...children } : null;
 }
